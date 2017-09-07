6604825add643269347201d033ca4ae76f374839
Nate Seeley <seeleyn@adaptivecomputing.com>
2017-09-07 17:41:28 -0600

    initial import

---
+++ src/main/java/com/acme/App.java
@@ -0,0 +1,17 @@
+package com.acme;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Welcome to Maven" );
+    }
+}
